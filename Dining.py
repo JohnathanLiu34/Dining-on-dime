2dbbe0ec86776e26c5db35cffe2863f7ca11ddba
Johnathan Liu <johnathanliu34@gmail.com>
2023-05-18 16:06:24 -0400

    stuff

--- Dining.py
+++ Dining.py
@@ -50,31 +50,44 @@ def get_place_details(api_key, place_id):
         "fields" : "name,price_leve",
         "key" : api_key
     }
-    request_status = requests.get(endpoint_url,params=param)
+    request_status = requests.get(endpoint_url,params = param)
     result = json.loads(request_status.content)
 
 
 def parse_price_level(desired_level):
     list = []
     restaurants = find_restaurants(API_Key,"40.74291,-73.87998")
+    restaurants = restaurants["results"]
     for restaurant in restaurants:
-        print(type(restaurant))
-        print(restaurant[2])
-        place_id = restaurant["place_id"]
-        details = get_place_details(API_Key, place_id)
-        if "price_level" in details["result"]:
-            if details["result"]["price_level"] == desired_level:
-                list.append(details["result"])
+        if "price_level" in restaurant:
+            if restaurant["price_level"] == desired_level:
+                list.append(restaurant)
+
+    return list
+
+def location_to_coordinates(location):
+    try:
+        endpoint_url = "https://maps.googleapis.com/maps/api/geocode/json?"
+        param = {
+            "address" : location,
+            "key" : API_Key,
+        }
+        request_status = requests.get(endpoint_url, params = param)
+        result = json.loads(request_status.content)['results'][0]['geometry']['location']
+        coords = str(result['lat'])+', '+str(result['lng'])
+        return coords
+
+    except Exception as e:
+        print(e)
+        return
 
 
 def main():
     print("Hello World")
     #print(get_user_location("Elmhurst NY"))
     #print(find_restaurants(API_Key,"40.74291,-73.87998"))
-    r = find_restaurants(API_Key,"40.74291,-73.87998")
-    print(r["place_id"])
-    #print(parse_price_level(1))
-
-
+    #r = find_restaurants(API_Key,"40.74291,-73.87998")
+    print(parse_price_level(1))
+    
 if __name__ == "__main__":
     main()
