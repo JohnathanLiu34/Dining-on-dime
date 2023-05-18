21b089a9cb88b61795fdaf48f8027c02dd5fdeee
amnzer <em.zerefa@gmail.com>
2023-05-18 15:37:45 -0700

    less bugs

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('home.html')
+@app.route('/howtouse')
+def howtouse():
+    return render_template('howtouse.html')  
 @app.route('/moreaboutyou')
 def moreaboutyou():
     return render_template('moreaboutyou.html')  
