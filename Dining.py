ed32b99b06acc5bbd54f72f91cff5509c27eacc1
Johnathan Liu <johnathanliu34@gmail.com>
2023-05-18 12:04:34 -0400

    new libraries

--- Dining.py
+++ Dining.py
@@ -2,11 +2,13 @@
 import googlemaps
 #pip install prettyprint
 import prettyprint
-
+import json
+import requests
+#from flask import Flask, render_template
 import time
 
 #Please do not share this, I am broke
-API_Key = "AIzaSyDLR544uFKruW4TKtE3TnJfXnjTBaRjDyY"
+API_Key = "AIzaSyAAsPXGsYgX8aF5O4HdP21AY-kbjMTJYsw"
 google_client  = googlemaps.Client(API_Key)
 
 def get_user_location(name):
@@ -22,10 +24,22 @@ def get_user_location(name):
         return
 
 
+def find_restaurants(api_key, location, radius = 1600):
+    endpoint_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?"
+    params = {
+        "location" : location,
+        "radius" : radius,
+        "type" : "restaurant",
+        "key" : api_key,
+    }
+    result = json.loads(requests.get(endpoint_url,params=params).content)
+    return result
 
 def main():
     print("Hello World")
     print(get_user_location("Elmhurst NY"))
+
+
 
 if __name__ == "__main__":
     main()
